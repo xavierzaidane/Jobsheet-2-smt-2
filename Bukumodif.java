100af58d5c44340fc346196451c8a92cb491ed08
xavierzaidane <xavierzaidane@gmail.com>
2024-02-23 14:29:55 +0700

    bukumodif

---
+++ Bukumodif.java
@@ -0,0 +1,28 @@
+public class Bukumodif {
+
+        String judul, pengarang;
+        int halaman, stok, harga;
+     
+        void tampilInformasi(){
+         System.out.println("Judul:" + judul);
+         System.out.println("Pengarang:" + pengarang);
+         System.out.println("Jumlah Halaman: " + halaman);
+         System.out.println("Sisa stok: " + stok);
+         System.out.println("Harga: Rp " + harga);
+        }
+        void terjual(int jml) {
+             if (stok>0) {
+                 stok += jml;
+             }
+             else{
+                 
+             }
+        }
+         void restock(int jml) {
+             stok += jml;
+         }
+         void gantiHarga(int hrg) {
+             harga = hrg;
+         }
+     }
+     
