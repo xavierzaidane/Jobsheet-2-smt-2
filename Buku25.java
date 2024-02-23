2da7156b92d1075b1e2e9e4ff668813bd6f5b674
xavierzaidane <xavierzaidane@gmail.com>
2024-02-23 14:02:14 +0700

    first commit

---
+++ Buku25.java
@@ -0,0 +1,22 @@
+public class Buku25 {
+
+   String judul, pengarang;
+   int halaman, stok, harga;
+
+   void tampilInformasi(){
+    System.out.println("Judul:" + judul);
+    System.out.println("Pengarang:" + pengarang);
+    System.out.println("Jumlah Halaman: " + halaman);
+    System.out.println("Sisa stok: " + stok);
+    System.out.println("Harga: Rp " + harga);
+   }
+   void terjual(int jml) {
+        stok -= jml;
+   }
+    void restock(int jml) {
+        stok += jml;
+    }
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+}
